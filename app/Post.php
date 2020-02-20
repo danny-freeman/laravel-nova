5c67033907e1c442c0ed8490b2d4ba28f6729485
Danny Freeman <danny@dannyfreeman.co.uk>
2020-02-20 11:53:03 +0000

    Resource Relationships

--- app/Post.php
+++ app/Post.php
@@ -10,4 +10,15 @@ class Post extends Model
         'publish_at' => 'datetime',
         'publish_until' => 'datetime',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
+ 
