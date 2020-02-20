2b2b817bd2ecbe9cc022f109d218183e2bbe7171
Danny Freeman <danny@dannyfreeman.co.uk>
2020-02-20 11:26:46 +0000

    Resource fields

--- app/Nova/Post.php
+++ app/Nova/Post.php
@@ -3,7 +3,10 @@
 namespace App\Nova;
 
 use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Boolean;
+use Laravel\Nova\Fields\DateTime;
 use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Select;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Fields\Trix;
 use Laravel\Nova\Http\Requests\NovaRequest;
@@ -46,7 +49,19 @@ class Post extends Resource
 
             Text::make('Title'),
 
-            Trix::make('Body')
+            Trix::make('Body'),
+
+            DateTime::make('Publish At')->hideFromIndex(),
+
+            DateTime::make('Publish Until')->hideFromIndex(),
+
+            Boolean::make('Is Published'),
+
+            Select::make('Category')->options([
+                'tutorials' => 'Tutorials',
+                'news' => 'News',
+            ])->hideWhenUpdating()
+
         ];
     }
 
